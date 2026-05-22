286e779736823ff5db54406ab5879b10fac47516
Arran Montgomerie <arranjmontgomerie@gmail.com>
2026-05-22 16:43:28 +0100

    Setup begun, win32 entry point implemented

---
+++ WinThirtyTwoEngine/main.cpp
@@ -0,0 +1,11 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+// ^ To streamline the inclusion of windows.h
+#include <Windows.h>
+#include <string>
+static int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance) 
+{
+
+	return 0;
+}
